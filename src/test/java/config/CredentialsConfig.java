7f6658580f7d3641583f3133a39231fcc2bcea9f
TMaksyutov <dadojkeee@mail.ru>
2022-12-10 15:28:57 +0300

    added config

---
+++ src/test/java/config/CredentialsConfig.java
@@ -0,0 +1,14 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+
+@Config.Sources("classpath:credentials.properties")
+public interface CredentialsConfig extends Config {
+        String username();
+
+        String password();
+
+        String token();
+}
+
